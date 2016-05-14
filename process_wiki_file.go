33c1d70cc9173be85a1e783b46574759f771f0a3
Johannes Holzfuß <johannes@holzfuss.name>
2016-05-14 11:23:39 +0200

    Add bluemonday HTML sanitizer

--- process_wiki_file.go
+++ process_wiki_file.go
@@ -7,8 +7,13 @@ import (
 	"path/filepath"
 
 	"github.com/anknown/ahocorasick"
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
 )
+
+func linkArticles(inputHtml []byte, recognizer goahocorasick.Machine) []byte {
+	return inputHtml
+}
 
 func processWikiFile(inputDir string, outputDir string, fileName string, mode os.FileMode, recognizer goahocorasick.Machine) {
 	contents, err := ioutil.ReadFile(filepath.Join(inputDir, fileName))
@@ -17,11 +22,11 @@ func processWikiFile(inputDir string, outputDir string, fileName string, mode os
 	}
 
 	output := blackfriday.MarkdownCommon(contents)
-
-	// TODO: Link articles
+	linked := linkArticles(output, recognizer)
+	safe := bluemonday.UGCPolicy().SanitizeBytes(linked)
 
 	outPath := filepath.Join(outputDir, fileName+".html")
-	err = ioutil.WriteFile(outPath, []byte(output), mode)
+	err = ioutil.WriteFile(outPath, safe, mode)
 	if err != nil {
 		log.Fatalf("Error while writing to '%s': %s", outPath, err)
 	}
